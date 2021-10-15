65346fbad66df5c531071d810306412bf5d296fd
S0les <73446357+S0les@users.noreply.github.com>
2021-10-15 19:18:54 +0200

    Greedy solution for the TPS problem
    Added: functions.py, dataset.txt, greedy.py
    
    dataset.txt - file with base test case from http://www.cs.put.poznan.pl/mmachowiak/optkomb.php?opcja=lectures
    functions.py - a python script with functions
    greedy.py - a python script to run greedy algorithm
    
    Changes:
    - functions.py
        Added:
        * greedy_algorithm()
        * import_data()
        * get_closest()
        * distance_squared()

---
+++ greedy.py
@@ -0,0 +1,10 @@
+from functions import import_data, greedy_algorithm
+
+if __name__ == '__main__':
+    cities, amount_of_cities = import_data("dataset.txt")
+    best_route, best_length = greedy_algorithm(cities, amount_of_cities)
+
+    print("Answer is:")
+    print(best_route)
+    print("\nWith distance:")
+    print(best_length)
